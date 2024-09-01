7cff6afb7e48ee665e34b9d9bc82fe3277178918
venukumarthumati <thumativenukumar06@gmail.com>
2024-09-01 18:50:10 +0530

    trying with build fail for testing purpose

--- src/test/java/com/javapalace/Spring_Jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javapalace/Spring_Jenkins/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ class SpringJenkinsApplicationTests {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	@Test
-	public void contextLoads() {
+	public contextLoads() {
 
 		logger.info("Test case executing");
 		assertEquals(true, true);
